aaedcf00392f471426c708da47207c3e020e19ea
Joseph Stewart <JosephStewart1752@gmail.com>
2023-08-02 17:37:32 -0400

    Initial commit

--- MildChallenge.js
+++ MildChallenge.js
@@ -2,4 +2,13 @@
 //if the Number is even, and the String "odd" if the Number is odd. Hint: You may need to do some extra research on the remainder 
 //operator (%) to solve this challenge.
 
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    for (var i = 0; i < 10; i++) {
+        if (i % 2) {
+            console.log(numbers[i] + " is even")
 
+        }
+       else {
+        console.log(number[i] + "is odd")
+       }
+    }
